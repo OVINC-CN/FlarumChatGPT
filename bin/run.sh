10bb199ccd09cb7ba52e2a11389ea11a8ffe98b2
orenzhang <orenzhang@ovinc.cn>
2023-08-25 02:04:25 +0800

    chore: change run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 
-mkdir celery-logs
-mv logs/* celery-logs
-python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-nohup python manage.py celery worker -l INFO -f /usr/src/app/celery-logs/worker.log >/dev/null 2>&1 &
-nohup python manage.py celery beat -l INFO -f /usr/src/app/celery-logs/beat.log >/dev/null 2>&1 &
-uwsgi --ini /usr/src/app/uwsgi.ini --processes $UWSGI_PROCESSES --threads $UWSGI_THREADS -w wsgi.wsgi:application >/dev/null 2>&1
+nohup celery worker -c 1 -l INFO -f /usr/src/app/celery-logs/worker.log >/dev/null 2>&1 &
+nohup celery beat -l INFO -f /usr/src/app/celery-logs/beat.log >/dev/null 2>&1 &
+tail -f /dev/null
